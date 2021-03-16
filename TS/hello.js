9d92491fbc09acd0cef4116db13b42a0cc222630
思远 <1783522207@qq.com>
2021-03-17 02:51:01 +0800

    Update hello.js

--- TS/hello.js
+++ TS/hello.js
@@ -3,10 +3,13 @@ function copyFields(target, source) {
         target[id] = source[id];
     }
 
-    
+
     return target;
 }
 var x = { a: 1, b: 2, c: 3, d: 4 };
+
+
+
 
 console.log(copyFields(x, { b: 10, d: 20 }));
 
